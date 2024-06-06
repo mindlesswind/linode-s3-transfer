375afe9cd0d0a29bfd139f6a1cab69050b40f36c
ngkahoken <ngkahoken@gmail.com>
2024-06-06 11:42:47 +0800

    add multithread

--- index.mjs
+++ index.mjs
@@ -14,22 +14,23 @@ const linodeBucketUrl = process.env.LINODE_BUCKET_URL
 
 let fileNames = []
 
-try {
-    const s3Client = new S3Client(
-        {
-            region: process.env.S3_AWS_REGION,
-            credentials: {
-                accessKeyId: process.env.AWS_ACCESS_KEY_ID,
-                secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY
-            }
-        }
-    );
+const date = new Date();
 
-    const date = new Date();
+const s3Client = new S3Client(
+    {
+        region: process.env.S3_AWS_REGION,
+        credentials: {
+            accessKeyId: process.env.AWS_ACCESS_KEY_ID,
+            secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY
+        }
+    }
+);
+
+try {
     if (!fs.existsSync("logs")) await mkdir("logs");
 
-    logging(date, `${"=".repeat(20)}Start Transfer${"=".repeat(20)}`)
-    logging(date, `Starting Time: ${date.toISOString()}\n`)
+    logging(`${"=".repeat(20)}Start Transfer${"=".repeat(20)}`)
+    logging(`Starting Time: ${date.toISOString()}\n`)
 
     fs.createReadStream(csvFile)
         .pipe(csv())
@@ -38,63 +39,95 @@ try {
         })
         .on('end', async () => {
 
-            logging(date, `Read Ending Time: ${new Date().toISOString()}`)
+            logging(`Read Ending Time: ${new Date().toISOString()}`)
 
             const uniqueFileNamesSet = [...new Set(fileNames)]
-            const uniqueFileNames = Array.from(uniqueFileNamesSet)
+            let uniqueFileNames = Array.from(uniqueFileNamesSet)
 
-            logging(date, `Start Transfer Time: ${new Date().toISOString()}`)
-            logging(date, `${"=".repeat(50)}\n`)
+            logging(`Start Transfer Time: ${new Date().toISOString()}`)
+            logging(`${"=".repeat(50)}\n`)
 
-            for (let key of uniqueFileNames) {
-                const res = await fetch(path.join(linodeBucketUrl, key), {
-                    dispatcher: new Agent({
-                        bodyTimeout: 10 * 60e3, // 10 minutes
-                    })
-                })
+            while (uniqueFileNames.length > 0) {
 
-                if (!fs.existsSync("tmp")) await mkdir("tmp");
-                const destination = path.resolve("./tmp", key);
-                try {
-                    const fileStream = fs.createWriteStream(destination, { flags: 'wx' });
+                let processList = []
 
-                    const pipe = Readable.fromWeb(res.body).pipe(fileStream)
-                    const downloadResult = await new Promise((resolve, reject) => { pipe.on("finish", resolve); pipe.on("error", reject) });
-                } catch (err) {
-                    if (err.code === 'EEXIST') {
-                        logging(date, `File ${data.name} exists in parallel download, skip this time`)
+                for (let i = 0; i < process.env.UPLOAD_THREADS; i++) {
+                    if (uniqueFileNames.length > 0) {
+                        processList.push(transferFile(uniqueFileNames.shift(), date))
                     } else {
-                        console.error(err)
-                        throw err
+                        break
                     }
                 }
 
-                const uploadCommand = new PutObjectCommand({
-                    Bucket: process.env.S3_BUCKET_NAME,
-                    Key: key,
-                    Body: fs.createReadStream(destination),
-                    ContentType: mime.getType(destination)
+                const results = await Promise.all(processList)
+
+                results.forEach(result => {
+                    if (result.success) {
+                        logging(`File ${result.key} uploaded successfully`)
+                    } else {
+                        logging(`File ${result.key} failed to upload`)
+                    }
                 })
-                try {
-                    const uploadResult = await s3Client.send(uploadCommand)
-                    logging(date, `File ${key} uploaded successfully`)
-                    fs.unlinkSync(destination)
-                } catch (err) {
-                    fs.unlinkSync(destination)
-                    logging(date, `File ${key} failed to upload`)
-                }
             }
 
-            logging(date, `\n${"=".repeat(50)}\n`)
-            logging(date, `End Transfer Time: ${new Date().toISOString()}`)
+            logging(`\n${"=".repeat(50)}\n`)
+            logging(`End Transfer Time: ${new Date().toISOString()}`)
         });
 } catch (err) {
     console.error(err)
 }
 
+async function transferFile(key) {
+    try {
+        const res = await fetch(path.join(linodeBucketUrl, key), {
+            dispatcher: new Agent({
+                bodyTimeout: 10 * 60e3, // 10 minutes
+            })
+        })
 
+        if (!fs.existsSync("tmp")) await mkdir("tmp");
 
-function logging(date, string) {
+        const destination = path.resolve("./tmp", key);
+
+        try {
+            const fileStream = fs.createWriteStream(destination, { flags: 'wx' });
+
+            const pipe = Readable.fromWeb(res.body).pipe(fileStream)
+
+            const downloadResult = await new Promise((resolve, reject) => { pipe.on("finish", resolve); pipe.on("error", reject) });
+
+        } catch (err) {
+            if (err.code === 'EEXIST') {
+                logging(`File ${key} exists in parallel download, skip this time`)
+            } else {
+                throw err
+            }
+        }
+
+        const uploadCommand = new PutObjectCommand({
+            Bucket: process.env.S3_BUCKET_NAME,
+            Key: key,
+            Body: fs.createReadStream(destination),
+            ContentType: mime.getType(destination)
+        })
+
+        try {
+            const uploadResult = await s3Client.send(uploadCommand)
+            fs.unlinkSync(destination)
+        } catch (err) {
+            fs.unlinkSync(destination)
+            throw err
+        }
+
+        return { success: true , key: key};
+
+    } catch (error) {
+        logging(`Error: ${error.message}`)
+        return { success: false , key: key};
+    }
+}
+
+function logging(string) {
     console.log(`${string}`);
     fs.appendFileSync(path.resolve("./logs", `${date.toISOString()}.log`), `${string}\n`)
 }
